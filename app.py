5e7da5450251f4eaea098aaeef9c819260c446a0
Igor <igor.barkhatov@gmail.com>
2018-05-24 15:11:36 +0300

    SQL

--- app.py
+++ app.py
@@ -3,9 +3,40 @@ import os
 import random
 from flask import Flask, request
 from pymessenger.bot import Bot
+
+
+import pymysql.cursors
+
+# Connect to the database
+connection = pymysql.connect(host='mysql100.1gb.ru',
+                             user='gb_bkhv',
+                             password='957c3877sg',
+                             db='gb_bkhv',
+                             #charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+try:
+    with connection.cursor() as cursor:
+        # Create a new record
+        sql = "INSERT INTO `test` (`test`) VALUES (1)"
+        #cursor.execute(sql, ('webmaster@python.org', 'very-secret'))
+        cursor.execute(sql)
+
+    # connection is not autocommit by default. So you must commit to save
+    # your changes.
+    connection.commit()
     
+    #with connection.cursor() as cursor:
+        # Read a single record
+        #sql = "SELECT `id`, `password` FROM `users` WHERE `email`=%s"
+        #cursor.execute(sql, ('webmaster@python.org',))
+        #result = cursor.fetchone()
+        #print(result)
+finally:
+    connection.close()
+
     
-app = Flask(__name__)
+    app = Flask(__name__)
 #ACCESS_TOKEN = 'EAAB0TQP6ZCigBAKnnkRGoqbpDRPiXvj6WfkkpzRqH6zm8SMqZBYML74foLtH7TXRCZAYq5WeBYR1Q2eLnPuUUeagf1e1ZBa7pEewwwfTleQAatZCnSC1TQ9g7IYIwuIlZBs5MBdYNhpzHfhuQtY04TqIXS58zAOkYqqj4RznGjKwZDZD'
 #VERIFY_TOKEN = 'TESTINGTOKEN'
 ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
