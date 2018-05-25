e992583ac6cce81f6245543a5913e5c3f5ba4d9a
Igor <igor.barkhatov@gmail.com>
2018-05-25 23:32:14 +0300

    1

--- app.py
+++ app.py
@@ -38,6 +38,7 @@ def receive_message():
                 cur = conn.cursor()
                 #cur.execute("INSERT INTO bot_users VALUES ("+recipient_id+","+recipient_id+",["+response_sent_text+"])")
                 cur.execute("INSERT INTO bot_users VALUES (%s, %s, %s)", (recipient_id,"Stevie",""+message_text+""))
+                cur.execute("INSERT INTO user_features VALUES (%s)", (recipient_id))
                 conn.commit()
                     
                 if message['message'].get('text'):
