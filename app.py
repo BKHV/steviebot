8af4e0e90adfec337c38a30a7142f9a34fdad03e
Igor <igor.barkhatov@gmail.com>
2018-05-28 13:27:17 +0300

    1

--- app.py
+++ app.py
@@ -44,6 +44,17 @@ def receive_message():
                 try:
                     cur = conn.cursor()
                     try:
+                        cur.execute("INSERT INTO user_features VALUES (%s, %s, %s)", (recipient_id,"Stevie",""+message_text+""))
+                    except psycopg2.IntegrityError:
+                        conn.rollback()
+                    else:
+                        conn.commit()
+                    cur.close()
+                except:
+                    pass
+                try:
+                    cur = conn.cursor()
+                    try:
                         cur.execute("INSERT INTO bot_users VALUES (%s, %s, %s)", (recipient_id,"Stevie",""+message_text+""))
                     except psycopg2.IntegrityError:
                         conn.rollback()
