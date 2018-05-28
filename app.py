cbe4b32404e0a14514e5340df2851201da77b72d
Igor <igor.barkhatov@gmail.com>
2018-05-28 17:10:53 +0300

    1

--- app.py
+++ app.py
@@ -34,14 +34,9 @@ def receive_message():
                 recipient_id = message['sender']['id']
                 
                 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-                #try:
-                    #cur = conn.cursor()
-                    #cur.execute("INSERT INTO user_features VALUES (%s, %s, %s)", (recipient_id,"Stevie",""+message_text+""))
-                    #conn.commit()
-                #except:
-                    #pass
+                
                 cur = conn.cursor()
-                cur.execute("INSERT INTO bot_users VALUES (%s, %s, %s)", (recipient_id,"Stevie",""+response_sent_text+""))
+                cur.execute("INSERT INTO bot_users VALUES (%s, %s, %s)", (recipient_id,"Stevie",""+message_text+""))
                 conn.commit()
                     
                 if message['message'].get('text'):
