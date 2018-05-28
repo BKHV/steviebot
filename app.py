6fb1d952f1abffadbccf562195e01a2a880cb727
Igor <igor.barkhatov@gmail.com>
2018-05-28 17:22:04 +0300

    1

--- app.py
+++ app.py
@@ -32,6 +32,7 @@ def receive_message():
             if message.get('message'):
                 #Facebook Messenger ID for user so we know where to send response back to
                 recipient_id = message['sender']['id']
+                message_text = message['message']['text']
                 
                 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
                 
